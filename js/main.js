96034841a3f5c0f06a6a72accc1df0ee9f1d01f5
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-10-28 17:37:21 +0200

    adds changes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
-const userName = document.getElementById("user-name");
-const distanceKm = document.getElementById("km");
-const pricePerKm = distanceKm * 0.21;
-const ageDiscount = document.getElementById("age-discount");
-const totalPrice = pricePerKm;
-const minorenne = document.getElementById("minorenne");
-const maggiorenne = document.getElementById("maggiorenne");
-const over65 = document.getElementById("over65");
-const numPriceDiscount20 =totalPrice * 0.8;
-const numPriceDiscount40 =totalPrice * 0.6;
-const ticketEl = document.querySelector(".price-ticket")
 
-if (ageDiscount === minorenne) {
-    ticketEl.innerHTML = "Costo biglietto € " + numPriceDiscount20.toFixed(2);
-} else if (ageDiscount === maggiorenne) {
-    ticketEl.innerHTML = "Costo biglietto € " + totalPrice.toFixed(2);
-} else if (ageDiscount === over65){
-    ticketEl.innerHTML = "Costo biglietto € " + numPriceDiscount40.toFixed(2);
+
+const ticketMakerEl = document.getElementById("ticket-maker");
+
+ticketMakerEl.addEventListener ("click", function(){
     
-}
+    const userName = document.getElementById("user-name");
+    const distanceKm = document.getElementById("km");
+    const pricePerKm = distanceKm * 0.21;
+    const ageDiscount = document.getElementById("age-discount");
+    const ageDiscountValue = ageDiscount.value;
+    const totalPrice = pricePerKm;
+    
+    const numPriceDiscount20 =totalPrice * 0.8;
+    const numPriceDiscount40 =totalPrice * 0.6;
+    const ticketEl = document.querySelector(".price-ticket");
+    
+    if (ageDiscountValue === "minorenne") {
+        ticketEl.innerHTML = userName + "Costo biglietto € " + numPriceDiscount20.toFixed(2);
+    } else if (ageDiscountValue === 'over65') {
+        ticketEl.innerHTML = userName + "Costo biglietto € " + numPriceDiscount40.toFixed(2); 
+    } else {
+        ticketEl.innerHTML = userName + "Costo biglietto € " + totalPrice.toFixed(2);
+    }
+
+})
 
 
