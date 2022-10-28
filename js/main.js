05db26ef228933839d7679f8b443a9fcdeb607c1
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-10-28 16:53:42 +0200

    adds changes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
-let age ;
-let percorso ;
+const userName = document.getElementById("user-name");
+const distanceKm = document.getElementById("km");
+const pricePerKm = distanceKm * 0.21;
+const ageDiscount = document.getElementById("age-discount");
+const totalPrice = pricePerKm;
+const minorenne = document.getElementById("minorenne");
+const maggiorenne = document.getElementById("maggiorenne");
+const over65 = document.getElementById("over65");
+const numPriceDiscount20 =totalPrice * 0.8;
+const numPriceDiscount40 =totalPrice * 0.6;
 
-const pricePerKm = 0.21;
-const distance = percorso;
-
-const totalPrice = (pricePerKm * distance);
-const numPriceDiscount20 = (totalPrice * 0.8 );
-const numPriceDiscount40 = (totalPrice * 0.6);
-
-
-if (age < 18) {
+if (ageDiscount === minorenne) {
     console.log("Il passegero ha uno sconto del 20% sul biglietto");
     console.log(" Costo biglietto € " + numPriceDiscount20.toFixed(2));
-} else if (age >= 18 && age < 65) {
+} else if (ageDiscount === maggiorenne) {
     console.log(" Costo biglietto € " + totalPrice.toFixed(2));
 
-} else if (age > 65){
+} else if (ageDiscount === over65){
     console.log("Il passeggero ha uno sconto del 40% sul biglietto");
     console.log(" Costo biglietto € " + numPriceDiscount40.toFixed(2));
 }
+
 
