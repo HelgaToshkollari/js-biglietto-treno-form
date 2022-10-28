d9eef8c48218c4dc288dfa0d672e0b8a458ffa81
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-10-28 17:04:00 +0200

    adds changes

--- js/main.js
+++ js/main.js
@@ -8,16 +8,15 @@ const maggiorenne = document.getElementById("maggiorenne");
 const over65 = document.getElementById("over65");
 const numPriceDiscount20 =totalPrice * 0.8;
 const numPriceDiscount40 =totalPrice * 0.6;
+const ticketEl = document.querySelector(".price-ticket")
 
 if (ageDiscount === minorenne) {
-    console.log("Il passegero ha uno sconto del 20% sul biglietto");
-    console.log(" Costo biglietto € " + numPriceDiscount20.toFixed(2));
+    ticketEl.innerHTML = "Costo biglietto € " + numPriceDiscount20.toFixed(2);
 } else if (ageDiscount === maggiorenne) {
-    console.log(" Costo biglietto € " + totalPrice.toFixed(2));
-
+    ticketEl.innerHTML = "Costo biglietto € " + totalPrice.toFixed(2);
 } else if (ageDiscount === over65){
-    console.log("Il passeggero ha uno sconto del 40% sul biglietto");
-    console.log(" Costo biglietto € " + numPriceDiscount40.toFixed(2));
+    ticketEl.innerHTML = "Costo biglietto € " + numPriceDiscount40.toFixed(2);
+    
 }
 
 
