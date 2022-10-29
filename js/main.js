ecca1e2bdedea763fed677599de40520dd8e2192
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-10-29 13:21:55 +0200

    adds changes

--- js/main.js
+++ js/main.js
@@ -6,21 +6,39 @@ ticketMakerEl.addEventListener ("click", function(){
     
     const userName = document.getElementById("name").value;
     const distanceKm = document.getElementById("km").value;
-    const pricePerKm = Math.floor(distanceKm * 0.21);
+    
     const ageDiscount = document.getElementById("age-discount");
     const ageDiscountValue = ageDiscount.value;
+
+    const pricePerKm = Math.floor(distanceKm * 0.21);
     const totalPrice = pricePerKm;
-    const numPriceDiscount20 = totalPrice * 0.8;
-    const numPriceDiscount40 = totalPrice * 0.6;
-    const ticketEl = document.querySelector(".price-ticket");
+    let discount;
+    let discountType;
+    
+    
     
     if (ageDiscountValue === "minorenne") {
-        ticketEl.innerHTML ="Costo biglietto € " + numPriceDiscount20.toFixed(2);
-    } else if (ageDiscountValue === 'over65') {
-        ticketEl.innerHTML = "Costo biglietto € " + numPriceDiscount40.toFixed(2); 
+        discount = totalPrice * 0.8;
+        discountType = "Sconto Minorenni";
+    } else if (ageDiscountValue === "over65") {
+        discount = totalPrice * 0.6;
+        discountType = "Sconto Over65";
     } else {
-        ticketEl.innerHTML ="Costo biglietto € " + totalPrice.toFixed(2);
+        discount = 0 ;
+        discountType = "Biglietto Standard";
     }
+    
+    const finalTicket = totalPrice - discount;
+    document.getElementById("passenger").innerHTML = userName;
+    document.getElementById("discount").innerHTML = discountType;
+    document.querySelector(".price-ticket").innerHTML = finalTicket.toFixed(2) + "€";
+    
+    const numCarozza= Math.floor(Math.random() * 9) + 1;
+    document.getElementById("carozza").innerHTML = numCarozza;
+
+    const codiceCp = Math.floor(Math.random() * 10000) + 90000;
+    document.getElementById("codice-cp").innerHTML = codiceCp;
+
 
 })
 
